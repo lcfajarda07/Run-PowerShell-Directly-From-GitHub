71ba906be8556062a8732457af1f8c150b1ab02c
lcfajarda07 <58523332+lcfajarda07@users.noreply.github.com>
2022-04-29 15:48:35 +0800

    Update Run-FromGitHub-SamplePowerShell.ps1

--- Run-FromGitHub-SamplePowerShell.ps1
+++ Run-FromGitHub-SamplePowerShell.ps1
@@ -14,9 +14,11 @@
 # Invoke-Expression $($ScriptFromGithHub.Content)
 
 
-$Ip = Read-Host -Prompt "Ip Address"
+Read-Host -Prompt "Ip Address"
 
-ping $Ip
+
+
+
 Write-Host "Here are the top 10 CPU consuming processes right now"
 Write-Host " "
 Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
