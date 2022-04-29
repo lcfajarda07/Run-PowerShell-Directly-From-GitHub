40e2a9fc9d76dc180a29e6dc18eca01e30cdf118
lcfajarda07 <58523332+lcfajarda07@users.noreply.github.com>
2022-04-29 15:18:12 +0800

    Add files via upload

---
+++ Run-FromGithub.ps1
@@ -0,0 +1,4 @@
+﻿
+$ScriptFromGithHub = Invoke-WebRequest https://raw.githubusercontent.com/tomarbuthnot/Run-PowerShell-Directly-From-GitHub/master/Run-FromGitHub-SamplePowerShell.ps1
+Invoke-Expression $($ScriptFromGithHub.Content)
+
