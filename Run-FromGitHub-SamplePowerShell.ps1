703838287a6d073b69bc5b4b281cc416d08cd0f8
lcfajarda07 <58523332+lcfajarda07@users.noreply.github.com>
2022-04-29 15:20:42 +0800

    Update Run-FromGitHub-SamplePowerShell.ps1

--- Run-FromGitHub-SamplePowerShell.ps1
+++ Run-FromGitHub-SamplePowerShell.ps1
@@ -19,3 +19,7 @@ Write-Host "Here are the top 10 CPU consuming processes right now"
 Write-Host " "
 Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
 
+echo Enter your Id : 
+set /p Id=
+
+ping %Id%
